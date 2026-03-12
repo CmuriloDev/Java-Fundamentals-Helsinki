0190fc09ebbf6a8567bef61f7697b77a6634cced
Carlos Murilo Nogueira Portela <murilo.portela.dev@gmail.com>
2026-03-12 10:46:32 -0300

    Java Programming 1: Lesson 4

---
+++ src/main/java/com/java/helsinki/part01/Ex04_Dinosaur/Dinosaur.java
@@ -0,0 +1,11 @@
+package com.java.helsinki.part01.Ex04_Dinosaur;
+
+public class Dinosaur {
+
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a dinosaur");
+    }
+}
