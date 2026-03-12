b6f87da1710deccdde58686293d5ffe74d8a63c9
Carlos Murilo Nogueira Portela <murilo.portela.dev@gmail.com>
2026-03-12 09:53:37 -0300

    Java Programming 1: Lesson 2

---
+++ src/main/java/com/java/helsinki/part01/AdaLovelace.java
@@ -0,0 +1,9 @@
+package com.java.helsinki.part01;
+
+public class AdaLovelace {
+
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Ada Lovelace");
+    }
+}
